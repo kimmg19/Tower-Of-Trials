35ae005a38b1082f25be4bff066f98a613b3cf27
dalskan <rsj6612@naver.com>
2024-05-22 03:38:21 +0900

    hp,stamina바 UI, 구르기 스태미나 시스템, 폰트 수정

--- Assets/Scripts/PlayerScripts/PlayerInputs.cs
+++ Assets/Scripts/PlayerScripts/PlayerInputs.cs
@@ -9,19 +9,26 @@ public class PlayerInputs : MonoBehaviour
     AnimationEvent animationEvent;
 
     PlayerMovement playerMovement;
-    [HideInInspector]public Vector2 moveInput;
-    [HideInInspector] public bool isRunning=false;
+    PlayerStats playerStats;
+    PlayerUI playerUI;
+    PlayerStatus playerStatus;  // 추가
+    [HideInInspector] public Vector2 moveInput;
+    [HideInInspector] public bool isRunning = false;
     [HideInInspector] public bool isDodging;
     [HideInInspector] public bool isGPress;
     Animator animator;
+
     void Start()
     {
-        playerMovement=GetComponent<PlayerMovement>();
+        playerStats = GetComponent<PlayerStats>();
+        playerUI = GetComponent<PlayerUI>();
+        playerMovement = GetComponent<PlayerMovement>();
+        playerStatus = GetComponent<PlayerStatus>();  // 추가
         inGameCanvas = GameObject.Find("InGameCanvas");
         animationEvent = GetComponent<AnimationEvent>();
         animator = GetComponent<Animator>();
-
     }
+
     void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
@@ -40,25 +47,30 @@ public class PlayerInputs : MonoBehaviour
             animator.SetTrigger("Attack");
         }
     }
+
     void OnRoll()
     {
         if (moveInput.magnitude != 0 && !isDodging && playerMovement.characterController.isGrounded)
         {
-            isDodging = true;
-            AudioManager.instance.Play("PlayerRoll");
-            playerMovement.dodgeVec = playerMovement.CalculateMoveDirection().normalized;
-            animator.SetTrigger("Dodge");
-            playerMovement.characterController.center = new Vector3(0, 0.5f, 0);
-            playerMovement.characterController.height = 1f;
-            playerMovement.characterBody.rotation = Quaternion.LookRotation(playerMovement.dodgeVec);
+            if (playerStats.currentStamina >= 15) // 스태미나가 충분한지 확인
+            {
+                playerStatus.UseStamina(15);  // 스태미나 감소
+                isDodging = true;
+                AudioManager.instance.Play("PlayerRoll");
+                playerMovement.dodgeVec = playerMovement.CalculateMoveDirection().normalized;
+                animator.SetTrigger("Dodge");
+                playerMovement.characterController.center = new Vector3(0, 0.5f, 0);
+                playerMovement.characterController.height = 1f;
+                playerMovement.characterBody.rotation = Quaternion.LookRotation(playerMovement.dodgeVec);
+            }
         }
     }
+
     void OnInteraction()
     {
-
         isGPress = true;
-
     }
+
     void OnPause()
     {
         inGameCanvas.GetComponent<InGameCanvas>().ClickPuaseButton();
